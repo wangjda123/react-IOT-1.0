944bd9634913599dd6d8790baf82957b3c51a969
wangjda123 <46208687+wangjda123@users.noreply.github.com>
2020-09-14 16:29:55 +0800

    IOT1.0
    1111

---
+++ src/pages/login/aa 2.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class Aa extends Component {
+    render() {
+        return (
+            <div>
+                aa
+            </div>
+        )
+        ;
+    }
+}
